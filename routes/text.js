c3acfd48941e25539fd857bd8ead5b49aaad510c
fatuk <fatukk@gmail.com>
2017-02-12 19:08:08 +0300

    Refactor text route

--- routes/text.js
+++ routes/text.js
@@ -1,27 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var Canvas = require('canvas'),
-	Image = Canvas.Image,
-	canvas = new Canvas(200, 200),
-	ctx = canvas.getContext('2d');
+var Canvas = require('canvas');
 
-ctx.font = '30px Impact';
-ctx.rotate(.1);
-ctx.fillText('Awesome!', 50, 100);
-
-var textMsg = ctx.measureText('Awesome!');
-ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.lineTo(50 + textMsg.width, 102);
-ctx.stroke();
-
-var base64ImageData = canvas.toDataURL().split(',')[1];
-var img = new Buffer(base64ImageData, 'base64');
+var canvasWidth = 200;
+var canvasHeight = 200;
+var position = {
+	x: 20,
+	y: 100
+};
+var Image = Canvas.Image;
+var canvas = new Canvas(canvasWidth, canvasHeight);
+var ctx = canvas.getContext('2d');
 
 /* GET text page. */
 router.get('/', function(req, res, next) {
-	// res.send('text');
+	var queryParams = {
+		text: req.query.msg || 'Add param',
+		color: req.query.color || '#00ff00'
+	};
+
+	clearCanvas(canvas);
+	renderText(queryParams.text);
+	makeUnderline(queryParams.text);
+
+	var base64ImageData = canvas.toDataURL().split(',')[1];
+	var img = new Buffer(base64ImageData, 'base64');
+
 	res.writeHead(200, {
 		'Content-Type': 'image/png',
 		'Content-Length': img.length
@@ -29,5 +33,27 @@ router.get('/', function(req, res, next) {
 
 	res.end(img);
 });
+
+function clearCanvas(_canvas) {
+	_canvas.getContext('2d').clearRect(0, 0, _canvas.width, _canvas.height);
+}
+
+function makeUnderline(_text) {
+	var textMsg = ctx.measureText(_text);
+
+	ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+	ctx.lineTo(position.x, position.y + ctx.lineWidth);
+
+	ctx.lineTo(position.x + textMsg.width, position.y + ctx.lineWidth);
+	ctx.stroke();
+}
+
+function renderText(_text) {
+	ctx.font = '30px Impact';
+	// ctx.fillStyle = queryParams.color;
+	ctx.fillText(_text, position.x, position.y);
+}
 
 module.exports = router;
