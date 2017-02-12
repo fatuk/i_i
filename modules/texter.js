f5ca08e9ca870d2731e1af0a013a087789892c76
fatuk <fatukk@gmail.com>
2017-02-13 00:42:27 +0300

    Implement texter

---
+++ modules/texter.js
@@ -0,0 +1,33 @@
+var Canvas = require('canvas');
+
+var Texter = function (_w, _h) {
+	var texter = {
+		width: _w || 200,
+		height: _h || 200,
+		x: 10,
+		y: 100,
+		color: 'green',
+		getImage: getImage,
+		clearCanvas: clearCanvas
+	};
+	texter.canvas = new Canvas(texter.width, texter.height);
+	texter.ctx = texter.canvas.getContext('2d');
+
+	return texter;
+};
+
+function getImage(_text) {
+	this.ctx.font = '30px Impact';
+	this.ctx.fillStyle = this.color;
+	this.ctx.fillText(_text, this.x, this.y);
+
+	var base64ImageData = this.canvas.toDataURL().split(',')[1];
+	this.img = new Buffer(base64ImageData, 'base64');
+	return this.img;
+}
+
+function clearCanvas() {
+	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+module.exports = Texter;
--- routes/text.js
+++ routes/text.js
@@ -1,59 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var Canvas = require('canvas');
 
-var canvasWidth = 200;
-var canvasHeight = 200;
-var position = {
+var Texter = require('../modules/texter');
+
+/*var position = {
 	x: 20,
 	y: 100
 };
-var Image = Canvas.Image;
 var canvas = new Canvas(canvasWidth, canvasHeight);
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');*/
 
 /* GET text page. */
 router.get('/', function(req, res, next) {
 	var queryParams = {
 		text: req.query.msg || 'Add param',
-		color: req.query.color || '#00ff00'
+		color: req.query.color || '#00ff00',
+		width: parseInt(req.query.w, 10) || 200,
+		height: parseInt(req.query.h, 10) || 200
 	};
-
-	clearCanvas(canvas);
-	renderText(queryParams.text);
-	makeUnderline(queryParams.text);
-
-	var base64ImageData = canvas.toDataURL().split(',')[1];
-	var img = new Buffer(base64ImageData, 'base64');
+	var texter = new Texter(queryParams.width, queryParams.height);
+	texter.color = queryParams.color;
+	texter.getImage(queryParams.text);
 
 	res.writeHead(200, {
 		'Content-Type': 'image/png',
-		'Content-Length': img.length
+		'Content-Length': texter.img.length
 	});
 
-	res.end(img);
+	res.end(texter.img);
 });
-
-function clearCanvas(_canvas) {
-	_canvas.getContext('2d').clearRect(0, 0, _canvas.width, _canvas.height);
-}
-
-function makeUnderline(_text) {
-	var textMsg = ctx.measureText(_text);
-
-	ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-	ctx.lineWidth = 2;
-	ctx.beginPath();
-	ctx.lineTo(position.x, position.y + ctx.lineWidth);
-
-	ctx.lineTo(position.x + textMsg.width, position.y + ctx.lineWidth);
-	ctx.stroke();
-}
-
-function renderText(_text) {
-	ctx.font = '30px Impact';
-	// ctx.fillStyle = queryParams.color;
-	ctx.fillText(_text, position.x, position.y);
-}
 
 module.exports = router;
