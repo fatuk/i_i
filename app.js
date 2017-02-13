fab11e974ed639837f84d0e5175200fe754c311f
fatuk <fatukk@gmail.com>
2017-02-13 03:08:46 +0300

    Change port

--- app.js
+++ app.js
@@ -6,18 +6,8 @@ var text = require('./routes/text');
 app.use('/', index);
 app.use('/text', text);
 
-
-/*app.get('/', function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'image/png',
-		'Content-Length': img.length
-	});
-
-	res.end(img);
-});*/
-
-app.listen(5000, function() {
-	console.log('App is listening to 5000 port');
+app.listen(8080, function() {
+	console.log('App is listening to 8080 port');
 });
 
 module.exports = app;
